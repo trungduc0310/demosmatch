3462dfd07a08d0d11467d3bb30a95480c12d5d8e
Duc Pham <duc.pham@micrales.vn>
2020-03-16 13:08:30 +0700

    update giaodien,dieuhuong1

--- app/src/main/java/com/example/phamtrungduc/demogiaodien/activity/Dangnhap.java
+++ app/src/main/java/com/example/phamtrungduc/demogiaodien/activity/Dangnhap.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,11 +13,12 @@ import android.widget.TextView;
 
 import com.example.phamtrungduc.demogiaodien.R;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Dangnhap extends AppCompatActivity {
     Button btndangnhap;
-    TextView tvdangky;
+    TextView tvdangky,tvforgotpass;
     EditText edtemail, edtpass;
     ImageButton btnfacebook;
 
@@ -40,11 +42,37 @@ public class Dangnhap extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        btndangnhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Dangnhap.this,Trangchu.class));
+            }
+        });
+        tvforgotpass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater=getLayoutInflater();
+                View view=inflater.inflate(R.layout.dialog_fogotpass,null);
+                Button btn_forgotpass=view.findViewById(R.id.btn_forgotpass);
+                btn_forgotpass.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
+                AlertDialog.Builder diaBuilder= new AlertDialog.Builder(Dangnhap.this);
+                diaBuilder.setView(view);
+                diaBuilder.setCancelable(true);
+                AlertDialog showDialog=diaBuilder.create();
+                showDialog.show();
+            }
+        });
     }
 
     private void Anhxa() {
         btndangnhap=findViewById(R.id.btn_dangnhap);
         tvdangky=findViewById(R.id.tv_dangky);
+        tvforgotpass=findViewById(R.id.tv_dangnhap_forgotpass);
         edtemail=findViewById(R.id.edt_dangnhap_email);
         edtpass=findViewById(R.id.edt_dangnhap_matkhau);
         btnfacebook=findViewById(R.id.imgbtn_facebook);
--- app/src/main/java/com/example/phamtrungduc/demogiaodien/fragment/Tuychon.java
+++ app/src/main/java/com/example/phamtrungduc/demogiaodien/fragment/Tuychon.java
@@ -1,23 +1,68 @@
 package com.example.phamtrungduc.demogiaodien.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.phamtrungduc.demogiaodien.R;
+import com.example.phamtrungduc.demogiaodien.activity.Dangnhap;
+import com.example.phamtrungduc.demogiaodien.activity.DoiPass;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Tuychon extends Fragment {
+    LinearLayout linearLayout;
+    TextView tv_logout,tv_changepass;
     public Tuychon() {
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tuychon,container,false);
+        View view=inflater.inflate(R.layout.fragment_tuychon,container,false);
+        Anhxa(view);
+        EventClick();
+        return view;
+    }
+
+    private void EventClick() {
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        tv_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Logout();
+            }
+        });
+        tv_changepass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChangePass();
+            }
+        });
+    }
+
+    private void Logout() {
+        startActivity(new Intent(getContext(),Dangnhap.class));
+    }
+
+    private void ChangePass() {
+        startActivity(new Intent(getContext(),DoiPass.class));
+    }
+
+    private void Anhxa(View view) {
+        linearLayout=view.findViewById(R.id.ln_fragmenttuychon_viewprifile);
+        tv_logout=view.findViewById(R.id.tv_tuychon_logout);
+        tv_changepass=view.findViewById(R.id.tv_tuychon_changepass);
     }
 }
